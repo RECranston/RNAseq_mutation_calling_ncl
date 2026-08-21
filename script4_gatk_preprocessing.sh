73fa89cfd54bd5b9c6cc4313ec9a1c31f76c9389
Ruth Cranston <116829725+RECranston@users.noreply.github.com>
2026-08-21 11:12:42 +0100

    Refactor reference genome handling in preprocessing script
    Updated reference genome handling for GATK preprocessing script to accommodate different assemblies. Adjusted paths and variables for reference files based on the selected assembly.

--- script4_gatk_preprocessing.sh
+++ script4_gatk_preprocessing.sh
@@ -21,14 +21,17 @@ example run: sbatch ./script4_gatk_preprocessing.sh sample_sheet.txt star_output
 
 # Set variables
 BASE_DIR="$PWD"
+ASSEMBLY="GRCh37"
 STAR_INDEX_DIR=${BASE_DIR}/"STAR_indexes/STAR_GRCh38"
+STAR_INDEX_DIR=${BASE_DIR}/STAR_indexes/STAR_${ASSEMBLY}
+REFERENCE_DIR=${BASE_DIR}/References/${ASSEMBLY}
+TMPDIR=${BASE_DIR}/tmp
+
 SAMPLE_SHEET=$1
 OUTPUT_DIR=${BASE_DIR}/$3
 STAR_DIR=${BASE_DIR}/$2
 PASS1_DIR=${STAR_DIR}"PASS1/"
 PASS2_DIR=${STAR_DIR}"PASS2/"
-REFERENCE_DIR=${BASE_DIR}/"References"
-TMPDIR=${BASE_DIR}/tmp
 
 # Load modules
 echo -en " * Loading modules...\n"
@@ -45,11 +48,20 @@ mkdir -p ${TMPDIR}
 LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${SAMPLE_SHEET})
 
 SAMPLE_ID=$(echo $LINE | awk '{print $1}')
-FILE1=$(echo $LINE | awk '{print $2}')
-FILE2=$(echo $LINE | awk '{print $3}')
 
 echo "Processing sample: ${SAMPLE_ID}"
 echo "Task ID: ${SLURM_ARRAY_TASK_ID}"
+
+# Set genome reference files
+if [ "${ASSEMBLY}" == "GRCh38" ]; then
+    REF_FASTA=${REFERENCE_DIR}/Homo_sapiens_assembly38.fasta
+    DBSNP=${REFERENCE_DIR}/Homo_sapiens_assembly38.dbsnp138.vcf
+    KNOWN_INDELS=${REFERENCE_DIR}/Homo_sapiens_assembly38.known_indels.vcf.gz
+else
+    REF_FASTA=${REFERENCE_DIR}/Homo_sapiens_assembly19.fasta
+    DBSNP=${REFERENCE_DIR}/dbsnp_138.b37.vcf.gz
+    KNOWN_INDELS=${REFERENCE_DIR}/Mills_and_1000G_gold_standard.indels.b37.vcf
+fi
 
 # Mark duplicates
 gatk MarkDuplicates \
@@ -65,7 +77,7 @@ echo -ne "*** Mark duplicates done! ***\n"
 # Split reads at splice junctions
 gatk SplitNCigarReads \
      --java-options "-Xmx90g -Djava.io.tmpdir=${TMPDIR}" \
-     -R ${REFERENCE_DIR}/Homo_sapiens_assembly38.fasta \
+     -R ${REF_FASTA} \
      -I ${OUTPUT_DIR}${SAMPLE_ID}_marked_dup.bam \
      -O ${OUTPUT_DIR}${SAMPLE_ID}_split.bam \
      --tmp-dir ${TMPDIR}
@@ -82,10 +94,10 @@ echo -ne "*** Split reads done! ***\n"
 # BQSR (use dbSNP + known indels)
 gatk BaseRecalibrator \
      --java-options "-Xmx90g" \
-     -R ${REFERENCE_DIR}/Homo_sapiens_assembly38.fasta \
+     -R ${REF_FASTA} \
      -I ${OUTPUT_DIR}${SAMPLE_ID}_split.bam \
-     --known-sites ${REFERENCE_DIR}/Homo_sapiens_assembly38.dbsnp138.vcf \
-     --known-sites ${REFERENCE_DIR}/Homo_sapiens_assembly38.known_indels.vcf.gz \
+     --known-sites ${DBSNP} \
+     --known-sites ${KNOWN_INDELS} \
      -O ${OUTPUT_DIR}${SAMPLE_ID}_recal.table
 
 echo -ne "*** BaseRecalibrator done! ***\n"
@@ -93,7 +105,7 @@ echo -ne "*** BaseRecalibrator done! ***\n"
 
 gatk ApplyBQSR \
      --java-options "-Xmx90g -Djava.io.tmpdir=${TMPDIR}" \
-     -R ${REFERENCE_DIR}/Homo_sapiens_assembly38.fasta \
+     -R ${REF_FASTA} \
      -I ${OUTPUT_DIR}${SAMPLE_ID}_split.bam \
      --bqsr-recal-file ${OUTPUT_DIR}${SAMPLE_ID}_recal.table \
      -O ${OUTPUT_DIR}${SAMPLE_ID}_recal.bam \
